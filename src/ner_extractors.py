8926d927e91522eac561b46cf96d4ef1fb44ac15
Josh Hook <joshhook@live.co.uk>
2021-03-01 15:27:21 +0000

    Added nlp_submission file and created extracting modules for regex and ner code

---
+++ src/ner_extractors.py
@@ -0,0 +1,10 @@
+
+def train_crf_ner_model(dataset):
+    """
+    Train a CRF Named Entity Recognition model for DATE, CARDINAL, ORDINAL, and NORP entities, using the given dataset.
+    """
+    pass
+
+
+def predict_named_entities():
+    pass
