a3f2135dfd72587ea3d7a3158ee7a8690d0d216a
Josh Hook <joshhook@live.co.uk>
2021-03-04 12:30:15 +0000

    Updated evaluation to measure f1 score, and started on regex questions extractor

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,23 +1,61 @@
+import os
+import sys
 import json
+
+import pandas as pd
+
+from sklearn.metrics import f1_score
 
 from regex_extractors import extract_table_of_contents, extract_questions
 
 
-def eval_regex_table_of_contents(file, expected: dict):
-    prediction = extract_table_of_contents(file)
+def eval_results(expected, prediction) -> float:
+    """ Evaluate predictions against expected, returning the f1 score """
     if len(prediction) != len(expected):
-        return False
+        print("\nPrediction len doesn't match expected len:", len(prediction), "!=", len(expected))
+        print("Prediction:", prediction)
+        print("Expected:", expected)
+        return f1_score(expected[:len(prediction)], prediction, average="macro")
 
-    for p, e in zip(prediction.items(), expected.items()):
+    for p, e in zip(prediction, expected):
         if p != e:
-            return False
+            print("Given:", p, ", but expected:", e)
 
-    return True
+    return f1_score(expected, prediction, average="macro")
+
+
+def main():
+    """ Read in all resources and evaluate """
+    resources_dir_path = sys.argv[1]
+    resources = os.listdir(resources_dir_path)
+
+    results_df = pd.DataFrame(columns=("Book", "ToC f1", "Questions f1", "NER f1", "Characters f1"))
+
+    for i, resource in enumerate(resources):
+        resource_path = os.path.join(resources_dir_path, resource)
+
+        # Regex table of contents
+        eval_path = os.path.join(resource_path, "eval_book.txt")
+        gold_path = os.path.join(resource_path, "gold_toc.json")
+        with open(eval_path, 'r', encoding="utf-8") as eval_file, open(gold_path, 'r', encoding="utf-8") as gold_file:
+            expected = [k + " " + v for k, v in json.load(gold_file).items()]
+            prediction = [k + " " + v for k, v in extract_table_of_contents(eval_file).items()]
+            toc_f1 = eval_results(expected, prediction)
+
+        # Regex questions
+        eval_path = os.path.join(resource_path, "eval_chapter.txt")
+        gold_path = os.path.join(resource_path, "gold_questions.txt")
+        with open(eval_path, 'r', encoding="utf-8") as eval_file, open(gold_path, 'r', encoding="utf-8") as gold_file:
+            expected = gold_file.read().split("\n")[:-1]
+            prediction = extract_questions(eval_file)
+            questions_f1 = eval_results(list(expected), list(prediction))
+
+        results_df.loc[i] = [resource, toc_f1, questions_f1, 0, 0]
+
+    print("\n" + results_df.sort_values(["ToC f1", "Questions f1", "NER f1", "Characters f1"]).to_markdown())
 
 
 if __name__ == "__main__":
-    with open("../resources/eval_book.txt", "r", encoding="utf-8") as book_file:
-        print(extract_table_of_contents(book_file))
+    """ Run with path to resources directory """
+    main()
 
-        with open("../resources/gold_toc.json", "r", encoding="utf-8") as expected_file:
-            print(eval_regex_table_of_contents(book_file, json.load(expected_file)))
--- src/regex_extractors.py
+++ src/regex_extractors.py
@@ -1,6 +1,8 @@
 import json
 import re
 
+from collections import OrderedDict
+
 
 def extract_table_of_contents(file, output_file: str = None) -> dict:
     """
@@ -8,14 +10,27 @@ def extract_table_of_contents(file, output_file: str = None) -> dict:
     table of contents, returning it as a JSON ({<chapter_number>: <chapter_title>}).
     If `output_file` is not None, then the JSON is outputted to a file with that name.
     """
-    book_contents = file.read()
-    chapter_pattern = r"""
-        (?xi) # Ignore case
-        chapter (\d+|[A-Z]+)[.:]?\s*([A-Z].*)\n
-    """
-    found_chapters = re.findall(chapter_pattern, book_contents)
 
-    table_of_contents = {num: title for num, title in found_chapters}
+    """
+    Things to consider:
+    * If a chapter has a number, but no name, e.g. "Chapter 2.", then this becomes {"2": ""} (empty title string)
+    * If the chapter heading is split across multiple lines, then don't include the "\n" in the title, just return the
+      text (replace the "\n" with a space (unless space already exists)), 
+      e.g. "Chapter 3.\nHey guys how \nAre you today" -> {"3": "Hey guys how Are you today"}
+    * Use `strip()` to remove any spaces at the start or end
+    * May be best to create multiple regex's, rather than one huge regex
+    * Use codex.open to read in text. This guarantees that text has UTF-8 chars in it. 
+    * DOTALL regex flag means that '.' will match everything, including newlines (doesn't normally match "\n")
+    * There will always be a chapter index
+    """
+    book_contents = file.read()
+
+    compiled_pattern = re.compile(r"C[hH][aA][pP][tT][eE][rR]\s+(\d+|[A-Z]+)[.:]? *\n?([A-Z].*)?\s*")
+    found_chapters = re.findall(compiled_pattern, book_contents)
+
+    table_of_contents = OrderedDict()
+    for num, title in found_chapters:
+        table_of_contents[num] = title
 
     # Save the file to disk
     if output_file is not None:
@@ -26,18 +41,26 @@ def extract_table_of_contents(file, output_file: str = None) -> dict:
     return table_of_contents
 
 
-def extract_questions(file, output_file: str = None) -> set:
+def extract_questions(file, output_file: str = None) -> list:
     """
     Given the file of a plain text book (from www.gutenberg.org), this extracts every question in the book, returning
     a set of the questions.
     If `output_file` is not None, then the set of questions is outputted to a plain text file.
     """
-    questions = {}
+    book_contents = file.read()
+
+    compiled_pattern = re.compile(r"([A-Z][^!.\"“]*?\?)", re.DOTALL)
+    found_questions = re.findall(compiled_pattern, book_contents)
+    print(found_questions)
+
+    questions = OrderedDict()
+    for q in found_questions:
+        questions[re.sub(r"\s", " ", q)] = 1
 
     # Save the file to disk
     if output_file is not None:
-        with open(output_file + ".json", "w", encoding="utf-8") as save_file:
-            json_as_str = json.dumps(questions, indent=2)
-            save_file.write(json_as_str + "\n")
+        with open(output_file + ".txt", "w", encoding="utf-8") as save_file:
+            text = "\n".join(questions)
+            save_file.write(text + "\n")
 
-    return questions
+    return list(questions.keys())
